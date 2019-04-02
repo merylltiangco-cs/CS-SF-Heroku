b095d58de7a4af3bf8f13e313ef18375e164e786
Meryll Tiangco <meryll.tiangco@contentserv.com>
2019-04-02 14:28:18 +0800

    Fixed port number error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
+app.listen((process.env.PORT || 8000), () => {
   console.log('Example app listening on port 8000!')
 });
 
