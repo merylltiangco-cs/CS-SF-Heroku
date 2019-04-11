4141b944dc0430766ebe7f1b15210e2f391bdfe7
Meryll Tiangco <meryll.tiangco@contentserv.com>
2019-04-11 16:44:53 +0800

    product list and single product query

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -8,6 +8,7 @@ const dataEndpoint = process.env.DATA_ENDPOINT;
 const apiVersion = process.env.API_VERSION;
 const requestUrl = baseUrl + dataEndpoint + apiVersion;
 
+
 function auth(req, res) {
   const loginURL = baseUrl + grantService;
   const credString = new Buffer(userName + ':' + password + ':' + clientSecret);
@@ -20,25 +21,39 @@ function auth(req, res) {
 
   request.post(loginURL, { headers: headers })
     .then((tokenResponse) => {
+      console.log('OK');
       const accesToken = JSON.parse(tokenResponse);
       const requestHeader = {
         'Authorization': 'Bearer ' + accesToken.access_token,
         'Content-Type': 'application/json'
       };
+
       const data = {
         'query': {
           'match_all_query': {}
         },
         'select': '(**)'
       };
-      //  /products/sony-kdl-26n4000?expand=all&site_id=SiteGenesis
-      request.post(requestUrl + '/product_search', { headers: requestHeader, json: data })
+
+      request.post(requestUrl + '/product_search ', { headers: requestHeader, json: data })
         .then((response) => {
-          res.status(200).end(response);
+          // res.status(200).send(response.hits);
+          const productID = response.hits[0].id;
+          console.log('PRODUCTID:' + productID);
+          request.get(requestUrl + `/products/${productID}?expand=all&site_id=SiteGenesis`, { headers: requestHeader })
+            .then((productResponse) => {
+              console
+              res.status(200).send(productResponse);
+
+            }).catch((error) => {
+
+              res.status(error.statusCode).send(error.error.error_description);
+
+            });
 
         }).catch((error) => {
 
-          res.status(error.statusCode).send(error.error.message);
+          res.status(error.statusCode).send(error.error.error_description);
 
         });
     }).catch((error) => {
