a93b50bc8e11060ef28178addb5dd101eeea1973
Meryll Tiangco <meryll.tiangco@contentserv.com>
2019-05-06 18:50:41 +0800

    Fixed controller path

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const productController = require('./controllers/productController');
+const { getProducts, getProduct } = require('./controllers/productController');
 const { catchErrors } = require('./helpers/errorHandlers');
 
-router.get('/', catchErrors(productController.getProducts));
-router.get('/products', catchErrors(productController.getProducts));
-router.get('/product/:productId', catchErrors(productController.getProduct));
+router.get('/', catchErrors(getProducts));
+router.get('/products', catchErrors(getProducts));
+router.get('/product/:productId', catchErrors(getProduct));
 
 module.exports = router;
