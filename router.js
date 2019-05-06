df9a9a1f368bc471a9155e49c2b715a8289bc4b6
Meryll Tiangco <meryll.tiangco@contentserv.com>
2019-05-06 18:43:51 +0800

    Fixed error

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router();
 const productController = require('./controllers/productController');
-const authController = require('./controllers/authController');
 const { catchErrors } = require('./helpers/errorHandlers');
 
 router.get('/', catchErrors(productController.getProducts));
