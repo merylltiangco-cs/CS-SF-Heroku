bb70fde2818da3a23b979993d143f20bfbb9f033
Meryll Tiangco <merylltiangco@gmail.com>
2019-05-10 18:49:34 +0800

    updated templates

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -8,33 +8,16 @@ const requestUrl = baseUrl + dataEndpoint + apiVersion;
 const shopRequestUrl = baseUrl + '/s/' + siteId + shopEndpoint + apiVersion;
 const { catchErrors } = require('../helpers/errorHandlers');
 
-exports.getProducts = async (req, res) => {
+exports.getProducts = async (req, res, next) => {
     const headers = {
         'Authorization': 'Bearer ' + req.session.accessToken,
         'Content-Type': 'application/json' 
     };
-
-    const json = {
-        'query': {
-            'text_query': { 
-                'fields': ['catalog_id'], 
-                'search_phrase': 'contentserv-default-catalog' 
-                }
-        },
-        'expand': ['prices', 'images'],
-        'select': '(**)'
-    };
-
-    //request.post(requestUrl + `/product_search?site_id=${siteId}`, { headers: requestHeader, json: query })
-    //ORTHO-84672594563225
-    //ORTHO-84672594563229
-    //request.get(shopRequestUrl + `/product_search?client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers, json })
     request.get(shopRequestUrl + `/product_search?refine=cgid=contentserv-default-category&expand=images,prices,variations&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers })
         .then((productsResponse) => {
-            const products = productsResponse.hits;
             console.log('PRODUCT LIST');
-            res.json(productsResponse);
-            //res.render('products', { products });
+            //res.send(products);
+            res.render('products', { products:products.hits });
     });
 
 };
@@ -48,12 +31,11 @@ exports.getProduct = async (req, res, next) => {
             'Content-Type': 'application/json' 
         };
         console.log(`GET PRODUCT: ${productId}`);
-        request.get(shopRequestUrl + `/products/${productId}/prices?client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader })
+        request.get(shopRequestUrl + `/products/${productId}?expand=images,prices,variations&all_images=true&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader })
             .then((productResponse) => {
                 const product = JSON.parse(productResponse);
-                console.log(product);
-                res.json(product);
-                //res.render('product', { product, title: product.page_title['default'] });
+                //res.json(product);
+                res.render('product', { product, title: product.page_title['default'] });
         });
     }
 };
