d19f42d00cdb19f4ad96692ef4da86ef18683582
Meryll Tiangco <meryll.tiangco@contentserv.com>
2019-06-11 19:05:58 +0800

    changed product parsing and added variant styles

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -16,10 +16,10 @@ exports.getProducts = async (req, res, next) => {
     request.get(shopRequestUrl + `/product_search?count=200&refine=cgid=contentserv-default-category&expand=images,prices&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers })
         .then((productsResponse) => {
             const products = JSON.parse(productsResponse);
-            const filtered = products.hits.filter(product => product.hit_type=="product");
+            const filtered = products.hits.filter(product => product.hit_type=="master");
             console.log('PRODUCT LIST');
-            res.send(products);
-            //res.render('products', { products:filtered });
+            //res.send(products);
+            res.render('products', { products:filtered });
     });
 
 };
@@ -33,15 +33,50 @@ exports.getProduct = async (req, res, next) => {
             'Content-Type': 'application/json' 
         };
         console.log(`GET PRODUCT: ${productId}`);
-        request.get(shopRequestUrl + `/products/${productId}?expand=images,prices,variations&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader })
+        request.get(shopRequestUrl + `/products/${productId}?expand=images,prices,variations&all_images=true&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader })
             .then((productResponse) => {
-                const product = JSON.parse(productResponse);
-                //res.json(product);
-                res.render('product', { product, title: product.page_title['default'] });
+                const parse = JSON.parse(productResponse);
+                const imageGroup = parse.image_groups.filter(group => group.view_type=="large");
+                const productImages = imageGroup.shift();
+                const variants = getVariants(parse.variants, imageGroup);
+                const product = {
+                    id: parse.id,
+                    name: parse.name,
+                    brand: parse.brand,
+                    price: parse.price,
+                    short_desc: parse.short_description,
+                    long_desc: parse.long_description,
+                    images: productImages,
+                    variants: variants
+                };
+                res.json(product);
+                //res.render('product', { product, title: product.name });
         });
     }
 };
 
-//function getProductImages = async (req,res,next) => {
-
-//}
+getVariants = (variantData, imageGroup) => {
+    const variants = imageGroup.map(({images, variation_attributes}) => {
+        const item = {
+            variation_attribute : variation_attributes[0].values[0].value,
+            image: {
+                link: images[0].link,
+                title: variation_attributes[0].values[0].value
+            }
+        };
+        return item;
+    });
+    
+    const variantOptions = variantData.map(({product_id, variation_values}) => {
+        return variants.map( variant =>{
+            if(variant.variation_attributes == variation_values['ContentServ-Default-Color']){
+                const option = {
+                    product_id : product_id
+                };
+                variant.options.push(option);
+            }
+        });
+    });
+    
+    return variantOptions;
+};
