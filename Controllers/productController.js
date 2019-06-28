d48cca3b80f7bddbf982a483550ab11bd76c2451
Meryll Tiangco <meryll.tiangco@contentserv.com>
2019-06-28 17:57:34 +0800

    Variant rework

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -31,81 +31,86 @@ exports.getProduct = async (req, res, next) => {
             'Authorization': 'Bearer ' + req.session.accessToken,
             'Content-Type': 'application/json' 
         };
-        console.log(`GET PRODUCT: ${productId}`);
         request.get(shopRequestUrl + `/products/${productId}?expand=images,prices,variations,options&all_images=true&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader })
-            .then((productResponse) => {
+            .then(async(productResponse) => {
                 const parse = JSON.parse(productResponse);
-                const imageGroup = parse.image_groups.filter(group => group.view_type=="large");
+                const imageGroup = parse.image_groups.filter(group => group.view_type=="swatch");
                 const productImages = imageGroup.shift();
-                const productVariants = getVariants(parse.variants, imageGroup, req.session.accessToken);
+                let variants = await getVariants(parse.variants, imageGroup, req.session.accessToken);
                 const product = {
                     id: parse.id,
                     name: parse.name,
                     master: parse.master['_type']==='master' ? true : false,
                     brand: parse.brand,
                     price: parse.price,
-                    short_desc: parse.short_description.replace(/<[^>]+>/g, ''),
-                    long_desc: parse.long_description.replace(/<[^>]+>/g, ''),
+                    short_desc: parse.short_description? parse.short_description.replace(/<[^>]+>/g, '') : 'No Description',
+                    long_desc: parse.long_description? parse.long_description.replace(/<[^>]+>/g, '') : 'No Description',
                     images: productImages.images,
-                    variants: productVariants
+                    variants
                 };
-                res.json(product);
-                //res.render('product', { product, title: product.name });
+                //res.json(product);
+                res.render('product', { product, title: product.name, selectedVariant: product.variants[0] });
         });
     }
 };
 
-getVariants = (variantData, imageGroup, token) => {
-    let variants = imageGroup.map(({images, variation_attributes}) => {
+//Sort images,variants and options
+getVariants = async (variantData, imageGroup, token) => {
+    //Get image first
+    let variants = imageGroup.map(({images, variation_attributes}, index) => {
         const item = {
-            variation_attribute : variation_attributes[0].values[0].value,
+            variation_attribute: variation_attributes[0].values[0].value,
+            variation_name: '',
             image: {
                 link: images[0].link,
                 title: variation_attributes[0].values[0].value
             },
-            options: []
+            options_size: [],
+            options_material: [],
+            selected: index==0? true : false
         };
         return item;
     });
-    
-    variants = variants.map( variant =>{
-        variantData.map(({product_id, variation_values}) => {
-            const options = getProductOptions(product_id, token);
+    //Get product Options
+    variants = await Promise.all(variants.map(async variant =>{
+        await Promise.all(variantData.map( async({product_id, variation_values}) => {
+            //Get size and material
+            const options = await getProductOptions(product_id, token).then((productResponse) => {
+                const parse = JSON.parse(productResponse);
+                return {
+                    name: parse.name,
+                    size: parse.c_size,
+                    material: parse.c_material
+                };
+            });
+            //Assign values
             if(variation_values){
                 if(variant.variation_attribute === variation_values[Object.keys(variation_values)[0]]){
-                    const option = {
-                        product_id,
-                        size: options.size,
-                        material: options.material
-                    };
-                    variant.options.push(option);
+                    const sizeDuplicate = variant.options_size.filter(obj => {return obj.value === options.size});
+                    if(options.size && sizeDuplicate.length === 0){
+                        const size = {value: options.size};
+                        variant.options_size.push(size);
+                    }
+                    const materialDuplicate = variant.options_material.filter(obj => {return obj.value === options.material});
+                    if(options.material && materialDuplicate.length === 0){
+                        const material = {
+                            value: options.material
+                        };
+                        variant.options_material.push(material);
+                    }
+                    variant.variation_name = options.name;
                 }
             }
-        });
+        }));
         return variant;
-    });
+    }));
     return variants;
 };
 
 getProductOptions = (productId, token) =>{
-    try{
-        const requestHeader = {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json' 
-        };
-        console.log(`GET Variant: ${productId}`);
-        const option = request.get(shopRequestUrl + `/products/${productId}?client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader })
-            .then((productResponse) => {
-                const parse = JSON.parse(productResponse);
-                const attrib =  {
-                    size: parse.c_size,
-                    material: parse.c_material
-                };
-                return attrib;
-            });
-        console.log(option);
-        return option;
-    }catch(error){
-        console.log(error);
-    }
+    const requestHeader = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json' 
+    };
+    return request.get(shopRequestUrl + `/products/${productId}?client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, { headers: requestHeader });
 };
